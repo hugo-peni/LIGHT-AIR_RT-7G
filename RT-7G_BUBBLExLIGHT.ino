d43bfe34020f19af0f0dd023e430eacddb50702d
Hugo Penichou <86434428+hugo-peni@users.noreply.github.com>
2022-04-22 16:24:22 +0200

    Add files via upload

--- RT-7G_BUBBLExLIGHT.ino
+++ RT-7G_BUBBLExLIGHT.ino
@@ -5,13 +5,41 @@ int BUBBLE = 4;
 
 int trigg = 10;
 int THRO_LIGHT = 30;
-int THRO_BUBBLE = 255;
+int THRO_BUBBLE = 235;
 int cycle_count;
 
 int PREV_STATE_PINK, STATE_PINK;
 int PREV_STATE_GREEN, STATE_GREEN;
 int EN_LIGHT;
 int EN_BUBBLE;
+
+
+
+unsigned long T1, T2;
+unsigned long T1_FIRE = 4320000;
+unsigned long T2_FIRE =  100000;
+
+int BUBBLE_AUTH = 0, LIGHT_AUTH = 0;
+
+
+void POWER_BUBBLE(){
+   if(EN_BUBBLE){
+       analogWrite(BUBBLE, THRO_BUBBLE);       
+    }
+    else{
+       analogWrite(BUBBLE, 0);
+    }
+}
+
+void POWER_LIGHT(){
+   if(EN_LIGHT){
+       analogWrite(LIGHT, THRO_LIGHT);
+   } 
+   else{
+       analogWrite(LIGHT, 0);
+   }  
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -35,85 +63,54 @@ void loop() {
   STATE_GREEN = digitalRead(PUSH_GREEN);
 
   
-  if(PREV_STATE_PINK && !STATE_PINK){
+    if(PREV_STATE_PINK && !STATE_PINK){
       EN_LIGHT = !EN_LIGHT;
-      if(EN_LIGHT){
-        analogWrite(LIGHT, THRO_LIGHT);
+      /*if(!EN_LIGHT){
+        LIGHT_AUTH = 0;
       }
       else{
-        analogWrite(LIGHT, 0);
-      }
+        LIGHT_AUTH = 1;
+      }*/
+      LIGHT_AUTH = !LIGHT_AUTH;
+      T1 = 0;
+      POWER_LIGHT();
     }
 
     if(PREV_STATE_GREEN && !STATE_GREEN){
       EN_BUBBLE = !EN_BUBBLE;
-      if(EN_BUBBLE){
-        analogWrite(BUBBLE, THRO_BUBBLE);       
+      /*if(!EN_BUBBLE){
+        BUBBLE_AUTH = 0;
       }
       else{
-        analogWrite(BUBBLE, 0);
-      }
+        BUBBLE_AUTH = 1;
+      }*/
+      BUBBLE_AUTH = !BUBBLE_AUTH;
+      T2 = 0;
+      POWER_BUBBLE();
     }
 
-  
+    if(T1 == T1_FIRE && LIGHT_AUTH){
+      T1 = 0;
+      EN_LIGHT = !EN_LIGHT;
+      POWER_LIGHT();
+    }
+    if(T2 == T2_FIRE && BUBBLE_AUTH){
+      T2 = 0;
+      EN_BUBBLE = !EN_BUBBLE;
+      POWER_BUBBLE();
+    }
+    if(!LIGHT_AUTH){
+      //T1 = 0;
+      EN_LIGHT = 0;
+      POWER_LIGHT();
+    }
+    if(!BUBBLE_AUTH){
+      //T2 = 0;
+      EN_BUBBLE = 0;
+      POWER_BUBBLE();
+    }
+   
+  T1++;
+  T2++;
   delay(10);
 }
-
-/*void FALL_PINK(){
-    if(PREV_STATE_PINK && !STATE_PINK){
-      EN_LIGHT = !EN_LIGHT;
-      if(EN_LIGHT){
-        analogWrite(LIGHT, THRO_LIGHT);
-      }
-      else{
-        analogWrite(LIGHT, 0);
-      }
-    }
-}
-
-void FALL_GREEN(){
-    if(PREV_STATE_GREEN && !STATE_GREEN){
-      EN_BUBBLE = !EN_BUBBLE;
-      if(EN_BUBBLE){
-        analogWrite(BUBBLE, THRO_BUBBLE);
-        
-      }
-      else{
-        analogWrite(BUBBLE, 0);
-      }
-    }
-}
-
-/*void FALL_PINK(){
-  if(cycle_count > trigg){
-    PREV_STATE_PINK = STATE_PINK;
-    STATE_PINK = digitalRead(PUSH_PINK);
-    if(PREV_STATE_PINK && !STATE_PINK){
-      EN_LIGHT = !EN_LIGHT;
-      if(EN_LIGHT){
-        //analogWrite(LIGHT, THRO_LIGHT);
-      }
-      else{
-        //analogWrite(LIGHT, 0);
-      }
-    }
-  }
-}
-
-void FALL_GREEN(){
-  if(cycle_count > trigg){
-    cycle_count = 0;
-    PREV_STATE_GREEN = STATE_GREEN;
-    STATE_GREEN = digitalRead(PUSH_GREEN);
-    if(PREV_STATE_GREEN && !STATE_GREEN){
-      EN_BUBBLE = !EN_BUBBLE;
-      if(EN_BUBBLE){
-        //analogWrite(BUBBLE, THRO_BUBBLE);
-        
-      }
-      else{
-        //analogWrite(BUBBLE, 0);
-      }
-    }
-  }
-}*/
